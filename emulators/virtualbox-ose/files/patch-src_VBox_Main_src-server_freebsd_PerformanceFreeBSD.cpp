2d69331b423a09160b365f5148b3f558b6f64489
Rozhuk Ivan <rozhuk.im@gmail.com>
2020-12-23 09:08:59 +0300

    emulators/virtualbox-ose: change host freemem calculation algo
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=211432
    
    Private patch.

--- emulators/virtualbox-ose/files/patch-src_VBox_Main_src-server_freebsd_PerformanceFreeBSD.cpp
+++ emulators/virtualbox-ose/files/patch-src_VBox_Main_src-server_freebsd_PerformanceFreeBSD.cpp
@@ -1,24 +1,66 @@
 --- src/VBox/Main/src-server/freebsd/PerformanceFreeBSD.cpp.orig	2017-07-27 13:18:27 UTC
 +++ src/VBox/Main/src-server/freebsd/PerformanceFreeBSD.cpp
-@@ -80,8 +80,10 @@ int CollectorFreeBSD::getHostMemoryUsage(ULONG *total,
-     cbParameter = sizeof(cPagesMemInactive);
-     if (!sysctlbyname("vm.stats.vm.v_inactive_count", &cPagesMemInactive, &cbParameter, NULL, 0))
-         cProcessed++;
+@@ -58,45 +58,36 @@
+ 
+ int CollectorFreeBSD::getHostMemoryUsage(ULONG *total, ULONG *used, ULONG *available)
+ {
+-    int rc = VINF_SUCCESS;
+     u_long cbMemPhys = 0;
+     u_int cPagesMemFree = 0;
+-    u_int cPagesMemInactive = 0;
+     u_int cPagesMemCached = 0;
+     u_int cPagesMemUsed = 0;
+     int cbPage = 0;
+-    size_t cbParameter = sizeof(cbMemPhys);
+-    int cProcessed = 0;
+-
+-    if (!sysctlbyname("hw.physmem", &cbMemPhys, &cbParameter, NULL, 0))
+-        cProcessed++;
++    size_t cbParameter;
+ 
++    cbParameter = sizeof(cbMemPhys);
++    if (sysctlbyname("hw.physmem", &cbMemPhys, &cbParameter, NULL, 0))
++        return VERR_NOT_SUPPORTED;
+     cbParameter = sizeof(cPagesMemFree);
+-    if (!sysctlbyname("vm.stats.vm.v_free_count", &cPagesMemFree, &cbParameter, NULL, 0))
+-        cProcessed++;
++    if (sysctlbyname("vm.stats.vm.v_free_count", &cPagesMemFree, &cbParameter, NULL, 0))
++        return VERR_NOT_SUPPORTED;
+     cbParameter = sizeof(cPagesMemUsed);
+-    if (!sysctlbyname("vm.stats.vm.v_active_count", &cPagesMemUsed, &cbParameter, NULL, 0))
+-        cProcessed++;
+-    cbParameter = sizeof(cPagesMemInactive);
+-    if (!sysctlbyname("vm.stats.vm.v_inactive_count", &cPagesMemInactive, &cbParameter, NULL, 0))
+-        cProcessed++;
++    if (sysctlbyname("vm.stats.vm.v_active_count", &cPagesMemUsed, &cbParameter, NULL, 0))
++        return VERR_NOT_SUPPORTED;
 +#if __FreeBSD_version < 1200016
      cbParameter = sizeof(cPagesMemCached);
-     if (!sysctlbyname("vm.stats.vm.v_cache_count", &cPagesMemCached, &cbParameter, NULL, 0))
+-    if (!sysctlbyname("vm.stats.vm.v_cache_count", &cPagesMemCached, &cbParameter, NULL, 0))
+-        cProcessed++;
++    if (sysctlbyname("vm.stats.vm.v_cache_count", &cPagesMemCached, &cbParameter, NULL, 0))
++        return VERR_NOT_SUPPORTED;
 +#endif
-         cProcessed++;
      cbParameter = sizeof(cbPage);
-     if (!sysctlbyname("hw.pagesize", &cbPage, &cbParameter, NULL, 0))
-@@ -90,8 +92,8 @@ int CollectorFreeBSD::getHostMemoryUsage(ULONG *total,
-     if (cProcessed == 6)
-     {
-         *total     = cbMemPhys / _1K;
+-    if (!sysctlbyname("hw.pagesize", &cbPage, &cbParameter, NULL, 0))
+-        cProcessed++;
++    if (sysctlbyname("hw.pagesize", &cbPage, &cbParameter, NULL, 0))
++        return VERR_NOT_SUPPORTED;
+ 
+-    if (cProcessed == 6)
+-    {
+-        *total     = cbMemPhys / _1K;
 -        *used      = cPagesMemUsed * (cbPage / _1K);
 -        *available = (cPagesMemFree + cPagesMemInactive + cPagesMemCached ) * (cbPage / _1K);
-+        *used      = (uint64_t)cPagesMemUsed * (cbPage / _1K);
-+        *available = ((uint64_t)cPagesMemFree + cPagesMemInactive + cPagesMemCached) * (cbPage / _1K);
-     }
-     else
-         rc = VERR_NOT_SUPPORTED;
+-    }
+-    else
+-        rc = VERR_NOT_SUPPORTED;
++    *total     = cbMemPhys / _1K;
++    *used      = (ULONG64)cPagesMemUsed * (cbPage / _1K);
++    *available = ((ULONG64)cPagesMemFree + cPagesMemCached) * (cbPage / _1K);
+ 
+-    return rc;
++    return VINF_SUCCESS;
+ }
+ 
+ int CollectorFreeBSD::getProcessCpuLoad(RTPROCESS process, ULONG *user, ULONG *kernel)
