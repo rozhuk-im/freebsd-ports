581cc643e8ff681bf413399f338d2e259549ad72
Rozhuk Ivan <rozhuk.im@gmail.com>
2024-05-20 14:25:43 +0300

    editors/pragtical: improve port

---
+++ editors/pragtical/files/patch-subprojects_linenoise_linenoise_linenoise.cpp
@@ -0,0 +1,10 @@
+--- subprojects/linenoise/linenoise/linenoise.cpp
++++ subprojects/linenoise/linenoise/linenoise.cpp
+@@ -112,6 +112,7 @@
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/ioctl.h>
++#include <sys/stat.h>
+ #include <cctype>
+ #include <wctype.h>
+ 
